10d26988b528d0c0379edc8b50a7198e08de816c
AnarchyBurger <n.l.schneider@gmail.com>
2018-07-09 14:03:39 -0400

    update

---
+++ fs.js
@@ -0,0 +1,43 @@
+function fs() {
+    //check to see if browser supports fullscreen mode
+    var fullscreenEnabled = document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled;
+    //checks to see if anything is in fullscreen mode
+    var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+
+    if (fullscreenEnabled && !fullscreenElement) {
+        launchIntoFullscreen(document.documentElement);
+    } else {
+        exitFullscreen();
+    }
+
+    function launchIntoFullscreen(element) { 
+console.log("!");
+
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+
+
+
+    }
+
+    function exitFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+
+        document.getElementById('fullScreen').style.display = "none";
+        document.getElementById('normalScreen').style.display = "none";
+
+    }
+}
